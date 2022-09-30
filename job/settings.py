3691a0a55ff0293304a9f5edbeed83024adf9b36
ravinderatri <ravinderatri86@gmail.com>
2022-09-30 21:28:46 +0530

    read me

--- job/settings.py
+++ job/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'etdq)uvq=t0rc&ams5_ovn6w8bcwknjj0u97*(#n^(76x*+dr1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djobportal.herokuapp.com']
+ALLOWED_HOSTS = ['jobopps.herokuapp.com']
 
 
 # Application definition
