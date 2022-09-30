6a4e18ff25222dc7e65cb2d2d5a83a635922f239
ravinderatri <ravinderatri86@gmail.com>
2022-09-30 20:21:31 +0530

    job finder

---
+++ account/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from account.managers import CustomUserManager
+
+JOB_TYPE = (
+    ('M', "Male"),
+    ('F', "Female"),
+
+)
+
+ROLE = (
+    ('employer', "Employer"),
+    ('employee', "Employee"),
+)
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, blank=False,
+                              error_messages={
+                                  'unique': "A user with that email already exists.",
+                              })
+    role = models.CharField(choices=ROLE,  max_length=10)  
+    mobile = models.IntegerField(blank=True, null=True, unique=True)
+    gender = models.CharField(choices=JOB_TYPE, max_length=1)
+
+
+    USERNAME_FIELD = "email"     
+    REQUIRED_FIELDS = ['mobile']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.first_name+ ' ' + self.last_name
+    objects = CustomUserManager()
